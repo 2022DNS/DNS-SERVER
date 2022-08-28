8d33036df5afc6fd1f582e7151ec1fee560f2ee1
junanhouse <csd3000j@naver.com>
2022-08-29 08:05:44 +0900

    rds 연결 및 dockerfile 수정

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'tutorial.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dndd_database',
+        'USER': 'admin',
+        'PASSWORD': 'dnddmysql',
+        'HOST': 'dndd-database.cepxsqcwdqds.ap-northeast-2.rds.amazonaws.com',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': 'SET sql_mode="STRICT_TRANS_TABLES"'
+        }
     }
 }
 
