f96a9c048d7c0dbd56e7aa6b9fdefff2e6d4f1b8
junanhouse <csd3000j@naver.com>
2022-09-02 01:54:58 +0900

    dockerfile 수정

--- tutorial/quickstart/models.py
+++ tutorial/quickstart/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Road_info(models.Model):
+    id = models.IntegerField() # 위도 경도 다르더라도 같은 id로 묶으면 될듯?
+    longitude = models.IntegerField() # 경도
+    latitude = models.IntegerField() # 위도
+    count = models.IntegerField() # 졸음횟수
+
+class image:
+    id = models.IntegerField()
+    image_id = models.IntegerField()
