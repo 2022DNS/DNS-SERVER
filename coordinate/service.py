c6028e38efab7b054ef73d2d627873d7756215f2
junanhouse <csd3000j@naver.com>
2022-09-15 05:32:48 +0900

    service

--- coordinate/service.py
+++ coordinate/service.py
@@ -1,21 +1,31 @@
 from .models import Road
 from detection.drowsy_driving_detection import check_drowsy_driving_based_on_eye_condition
 
-def flo2str(flo):
+def changer(flo):
+
+    ##배성준 십년아 여기에 반올림하는 코드짜놔
     flo = str(flo).split(".")
     return flo[0] + '-' + flo[1]
 
 def run(request):
+
     res_code = check_drowsy_driving_based_on_eye_condition(request.get('images'))
-    if res_code == 1:
-        la = request.get('latitude')
-        lo = request.get('longitude')
+    if res_code != 1:
+        return res_code
+
+    la = changer(request.get('latitude'))
+    lo = changer(request.get('longitude'))
+
+    data = Road.objects.filter(longitude=lo, latitude=la)#.get()
+    if data.exists():
+        data.count = data.count+1
+        data.save()
+    else:
         Road.objects.create(
             latitude=la,
             longitude=lo
         )
-    else:
-        return res_code
+    return res_code
 
 
 
