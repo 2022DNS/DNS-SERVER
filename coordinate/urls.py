6e739b56fa987fdac4c7dea1ae08b4454a0c860e
Sohn Young Jin <sonkim1001@naver.com>
2022-09-15 17:55:04 +0900

    Fix wrong url error

--- coordinate/urls.py
+++ coordinate/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('dectection/', views.RoadListAPIView.as_view()),
+    path('coordinate/', views.RoadListAPIView.as_view()),
     path('coordinate/lo/<str:pk>/la/<str:post_pk>', views.RoadDetailAPIView.as_view()),
-    path('coordinate/create', views.RoadCreateAPIView.as_view())
+    path('detection', views.RoadCreateAPIView.as_view())
 ]
