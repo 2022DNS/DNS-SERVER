60781e2c9c526bef712082d9149b27e31696e661
junanhouse <csd3000j@naver.com>
2022-09-15 15:23:54 +0900

    .

--- dns/urls.py
+++ dns/urls.py
@@ -16,10 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
-from django.http import HttpResponse
-
 
 urlpatterns = [
-    path('/admin', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', include('coordinate.urls'))
 ]
